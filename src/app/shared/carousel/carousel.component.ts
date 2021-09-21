b82b74b54e54efb492cc2face55cbcdd7532bd13
Prince Darko <padarko99@gamil.com>
2021-09-20 19:04:15 -0700

    'hmm

--- src/app/shared/carousel/carousel.component.ts
+++ src/app/shared/carousel/carousel.component.ts
@@ -14,7 +14,7 @@ export class CarouselComponent implements OnInit {
 
   slides = [
     {'image': 'https://image.shutterstock.com/image-photo/black-nurse-portrait-260nw-232644115.jpg'}, 
-    {'image': '  https://us.123rf.com/450wm/rido/rido1704/rido170400095/76465313-portrait-of-smiling-doctor-with-stethoscope-around-his-neck-at-medical-clinic-happy-smiling-senior-d.jpg?ver=6'}, 
+    {'image': 'https://us.123rf.com/450wm/rido/rido1704/rido170400095/76465313-portrait-of-smiling-doctor-with-stethoscope-around-his-neck-at-medical-clinic-happy-smiling-senior-d.jpg?ver=6'}, 
     {'image': 'https://gsr.dev/material2-carousel/assets/demo.png'}
   ];
 
